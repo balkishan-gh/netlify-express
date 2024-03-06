5a6adb4949ea8f994d90d4c5fd4f8f16a272669f
Bal Kishan <bkunsolved@gmail.com>
2024-03-06 14:26:36 +0530

    add something

---
+++ netlify/fucntion/api.ts
@@ -0,0 +1,17 @@
+// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
+
+import express, { Router } from "express";
+import serverless from "serverless-http";
+import cors from "cors";
+
+const api = express();
+
+api.use(cors());
+api.use(express.json());
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+export const handler = serverless(api);
